ec40d6ba0b02afdd5ee828600c2095163bb21cea
Ryan <ryanccolson@gmail.com>
2022-08-16 08:17:48 -0700

    Cookie cache Implement a cache driver for cookie-based cache storage

--- tests/CookieCacheTest.php
+++ tests/CookieCacheTest.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace GrayMatterLabs\SimpleCache\Tests;
 
-use GrayMatterLabs\SimpleCache\ArrayCache;
+use GrayMatterLabs\SimpleCache\CookieCache;
 use Psr\SimpleCache\CacheInterface;
 
-class ArrayCacheTest extends BaseCacheTests
+/** @runTestsInSeparateProcesses enabled */
+class CookieCacheTest extends BaseCacheTests
 {
     public function getCache(): CacheInterface
     {
-        return new ArrayCache();
+        return new CookieCache('test');
     }
 }
